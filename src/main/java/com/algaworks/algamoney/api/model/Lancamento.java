d03cf9717f2310364fbc1a70307d42bca9a316c0
Breno Cabral <brcabral@gmail.com>
2026-08-21 15:24:19 -0300

    5.5. Validando lançamento com Bean Validation

--- src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
+++ src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
@@ -22,27 +23,33 @@ public class Lancamento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotNull
 	private String descricao;
 
+	@NotNull
 	@Column(name = "data_vencimento")
-	// @JsonFormat(pattern =  "dd/MM/yyyy") -> Caso queira formatar a data, adicionar essa anotação
+	// @JsonFormat(pattern = "dd/MM/yyyy") -> formatar a data
 	private LocalDate dataVencimento;
 
 	@Column(name = "data_pagamento")
-	// @JsonFormat(pattern =  "dd/MM/yyyy")
+	// @JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
 
 	private String observacao;
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
